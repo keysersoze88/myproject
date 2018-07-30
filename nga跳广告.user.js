0b6e882243a59cfd0747dbb3900374eb08e601d5
keysersoze88 <l498493006@gmail.com>
2018-07-30 12:08:51 +0800

    Update nga跳广告.user.js

--- nga跳广告.user.js
+++ nga跳广告.user.js
@@ -5,5 +5,6 @@
 // @grant       none
 // @match *://bbs.ngacn.cc/misc/adpage_insert_2.html?*
 // @match *://bbs.nga.cn/misc/adpage_insert_2.html?*
+// @match *://nga.donews.com/misc/adpage_insert_2.html?*
 // ==/UserScript==
 setTimeout("getJump()",1);
