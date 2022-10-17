5dd9524d546879b5f3015ebdfdabf19326623fe5
keysersoze88 <l498493006@gmail.com>
2022-10-17 23:00:19 +0800

    Update nga跳广告.user.js

--- nga跳广告.user.js
+++ nga跳广告.user.js
@@ -6,5 +6,6 @@
 // @match *://bbs.ngacn.cc/misc/adpage_insert_2.html?*
 // @match *://bbs.nga.cn/misc/adpage_insert_2.html?*
 // @match *://nga.donews.com/misc/adpage_insert_2.html?*
+// @match *://nga.178.com/misc/adpage_insert_2.html?*
 // ==/UserScript==
 setTimeout("getJump()",1);
